7d12523f566e8ee8f8683fff674e38fd2311c2d5
Justin Lathrop <jelathrop@gmail.com>
2015-08-02 14:49:18 -0400

    added the rest of the bin features, TODO: fix up the javascript loading order and include paths within modules

--- bin/cast-central-web.js
+++ bin/cast-central-web.js
@@ -3,49 +3,67 @@
 // CAST-CENTRAL-WEB
 // ----------------
 
-var sass = require('node-sass'),
+var sass       = require('node-sass'),
     compressor = require('node-minify'),
-    fs = require('fs'),
-    glob = require('glob'),
-    async = require('async');
+    fs         = require('fs'),
+    glob       = require('glob'),
+    async      = require('async'),
+    execSync   = require('exec-sync'),
+    express    = require('express'),
+    opts       = require('optimist')
+        .usage(
+            'Manages the cast-central-web application server.\n'+
+            'Usage: $0 {clean|build|start} <options>'
+        )
+        .describe('host', 'The hostname/ip of the server to bind to').default('host', 'localhost')
+        .describe('port', 'The port of the server to bind to').default('port', '8001')
+        .alias('c', 'clean').describe('c', 'Clean out the target directory compiled/compressed code')
+        .alias('b', 'build').describe('b', 'Build and compress the web code and place in the target dir')
+        .alias('r', 'run').describe('r', 'Start the web server using the compiled/compressed code')
+        .alias('d', 'dev').describe('d', 'Adds test dependencies to target dir')
+        .alias('t', 'target').describe('t', 'Target directory for building/cleaning/starting').default('t', 'target/')
+        .alias('h', 'help').describe('h', 'Shows this usage'),
+    argv = opts.argv;
 
 // When invoked will start a production server using 
 // minimized css, images, html, and js.
 
-async.series([
-    clean,
-    compile_sass,
-    compress_css,
-    compress_js
-], function(err, results){
-    run_server();
-});
+// Get current script location, all locations are 
+// relative to this location.
+var prefix = process.argv[1].split('/');
+prefix.pop();
+prefix = prefix.join('/') + '/../';
+
+// Process the options given
+if(argv.help){
+    opts.showHelp();
+    process.exit(0);
+}else{
+    async.series(get_steps(), function(err, results){
+        if(argv.run){ run_server(); }
+    });
+}
 
 // Clean the target dir
 function clean(cb){
     console.log('Cleaning...');
-    glob('target/*', function(files){
-        for(f in files){
-            fs.unlinkSync(files[f]);
-        }
-
-        cb(null);
-    });
+    execSync('rm -r '+prefix+argv.target+'*');
+    cb(null);
 }
 
 // Compile sass files
 function compile_sass(cb){
     console.log('Compiling sass...');
     var css = sass.renderSync({
-        file: 'resources/sass/style.scss',
+        file: prefix+'resources/sass/style.scss',
         includePaths: [
-            'resources/sass/breakpoints',
-            'resources/sass/modules',
-            'resources/sass/partials'
+            prefix+'resources/sass/breakpoints',
+            prefix+'resources/sass/modules',
+            prefix+'resources/sass/partials'
         ]
     });
 
-    fs.writeFileSync('target/style.compiled.css', css.css);
+    fs.writeFileSync(prefix+argv.target+'style.compiled.css', css.css);
     cb(null);
 }
 
@@ -55,12 +73,12 @@ function compress_js(cb){
     new compressor.minify({
         type: 'yui-js',
         fileIn: glob_arr_patterns([
-            'bower_components/**/*.min.js',
-            'app.js',
-            'config.js',
-            'modules/**/*.js'
+            prefix+'bower_components/**/*.min.js',
+            prefix+'app.js',
+            prefix+'config.js',
+            prefix+'modules/**/*.js'
         ]),
-        fileOut: 'target/app.min.js',
+        fileOut: prefix+argv.target+'app.min.js',
         callback: function(err, min){
             cb(err);
         }
@@ -73,28 +91,49 @@ function compress_css(cb){
     new compressor.minify({
         type: 'yui-css',
         fileIn: [
-            'target/style.compiled.css'
+            prefix+argv.target+'style.compiled.css'
         ],
-        fileOut: 'target/style.min.css',
+        fileOut: prefix+argv.target+'style.min.css',
         callback: function(err, min){
             cb(err);
         }
     });
 }
 
-// Compress Images
-function compress_images(cb){
+// Images
+function images(cb){
+    console.log('Moving images...');
+    execSync('cp -r '+prefix+'resources/images '+prefix+argv.target);
     cb(null);
 }
 
-// Compress HTML
-function compress_html(cb){
+// HTML
+function html(cb){
+    console.log('Moving html...');
+    execSync('cp '+prefix+'*.html '+prefix+'modules/**/*.html '+prefix+argv.target);
+    cb(null);
+}
+
+// Test
+function test(cb){
+    console.log('Moving test fixtures...');
+    execSync('cp -r '+prefix+'resources/data '+prefix+argv.target);
     cb(null);
 }
 
 // Run server
 function run_server(){
-    console.log('Starting server at <port>.');
+    console.log('Starting server at '+argv.host+':'+argv.port+'.');
+
+    var app    = express().use(express.static(prefix+argv.target)),
+        server = app.listen(argv.port);
+
+    /*if(argv.dev){
+        fs.watch(prefix, function(event, filename){
+            console.log('File change detected...')
+            async.series(get_steps(), function(err, results){  });
+        });
+    }*/
 }
 
 // Given an array of patterns, 
@@ -108,4 +147,27 @@ function glob_arr_patterns(patterns){
     }
 
     return(files);
+}
+
+// Uses the options given and returns 
+// the order of functions to call.
+function get_steps(){
+    var steps = [];
+
+    if(argv.clean){ steps.push(clean); }
+    if(argv.build){
+        steps.push(compile_sass);
+        steps.push(compress_css);
+        steps.push(compress_js);
+        steps.push(images);
+        steps.push(html);
+    }
+    if(argv.dev){ steps.push(test); }
+
+    if(steps.length === 0){
+        opts.showHelp();
+        process.exit(1);
+    }
+
+    return(steps);
 }
